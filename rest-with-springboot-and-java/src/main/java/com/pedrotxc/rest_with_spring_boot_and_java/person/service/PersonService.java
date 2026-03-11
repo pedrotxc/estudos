cc1b5355118450d7d3982fe8cf97918ad7fb4194
Pedro Henrique <pedrotxc@gmail.com>
2026-03-11 18:29:45 -0300

    mudando logs em packages

--- rest-with-springboot-and-java/src/main/java/com/pedrotxc/rest_with_spring_boot_and_java/person/service/PersonService.java
+++ rest-with-springboot-and-java/src/main/java/com/pedrotxc/rest_with_spring_boot_and_java/person/service/PersonService.java
@@ -3,12 +3,12 @@ package com.pedrotxc.rest_with_spring_boot_and_java.person.service;
 import com.pedrotxc.rest_with_spring_boot_and_java.exception.ResourceNotFoundException;
 import com.pedrotxc.rest_with_spring_boot_and_java.person.model.Person;
 import com.pedrotxc.rest_with_spring_boot_and_java.person.repository.PersonRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonService {
@@ -17,7 +17,7 @@ public class PersonService {
     PersonRepository repository;
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonService.class.getName());
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(PersonService.class.getName());
 
     public List<Person> findAll() {
         logger.info("Finding all People!");
